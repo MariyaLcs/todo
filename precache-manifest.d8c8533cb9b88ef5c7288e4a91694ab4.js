ead28bcc2b3ef97df072ec2d758ed86e751d3ba4
Mariya <mariya.podosinova@gmail.com>
2020-05-26 10:24:35 +0100

    Updates

---
+++ precache-manifest.d8c8533cb9b88ef5c7288e4a91694ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ee8258cf038860a69e685b1c310bf3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "40fdfdfd34bd77345614",
+    "url": "/todo/static/css/main.4b26b95d.chunk.css"
+  },
+  {
+    "revision": "9cf88d6455df5b6a696c",
+    "url": "/todo/static/js/2.14fb4be9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.14fb4be9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40fdfdfd34bd77345614",
+    "url": "/todo/static/js/main.c96cacdf.chunk.js"
+  },
+  {
+    "revision": "42e23f9118876525bea8",
+    "url": "/todo/static/js/runtime-main.0d77e128.js"
+  }
+]);
