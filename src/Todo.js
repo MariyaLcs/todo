0980213f3660e77c3b96ea228a8a58aaaa175ebe
Mariya <mariya.podosinova@gmail.com>
2020-05-26 08:56:43 +0100

    Editing Todos

--- src/Todo.js
+++ src/Todo.js
@@ -3,16 +3,66 @@ import React, { Component } from "react";
 class Todo extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isEditing: false,
+      task: this.props.task,
+    };
     this.handleRemove = this.handleRemove.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
   }
   handleRemove() {
     this.props.removeTodo(this.props.id);
   }
+  toggleForm() {
+    this.setState({ isEditing: !this.state.isEditing });
+  }
+  handleUpdate(evt) {
+    evt.preventDefault();
+    //take new task data and pass up to parent
+    this.props.updateTodo(this.props.id, this.state.task);
+    this.setState({ isEditing: false });
+  }
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value,
+    });
+  }
+  handleToggle(evt) {
+    this.props.toggleTodo(this.props.id);
+  }
   render() {
+    let result;
+    if (this.state.isEditing) {
+      result = (
+        <div>
+          <form className="Todo-edit-form" onSubmit={this.handleUpdate}>
+            <input
+              type="text"
+              value={this.state.task}
+              name="task"
+              onChange={this.handleChange}
+            />
+            <button>Save</button>
+          </form>
+        </div>
+      );
+    } else {
+      result = (
+        <div>
+          <li className="Todo-task" onClick={this.handleToggle}>
+            {this.props.task}
+          </li>
+        </div>
+      );
+    }
     return (
       <div>
-        <li>{this.props.task}</li>
-        <button>Edit</button>
+        {result}
+
+        <button onClick={this.toggleForm}>Edit</button>
         <button onClick={this.handleRemove}>X</button>
       </div>
     );
